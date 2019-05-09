3dc96c1fcd088cc04d1b27f65581ddf2b717bfc1
Clint Kunz <clintkunz@gmail.com>
2019-05-09 11:53:07 -0600

    top header complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 function Header() {
     return(
         <div className="header">
-            Header
+            <div className="title-search">
+                <div>
+                    <i class="fas fa-list-ul"></i>
+                    All Leads
+                </div>
+                <div className="search-field">
+                    <i class="fas fa-search"></i>
+                    <input 
+                        type="text"
+                        placeholder="Search Leads"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
